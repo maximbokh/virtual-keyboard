15917fce62bf07bb792bdfe81c2927d606bea580
maximbokh <isupermaximus@gmail.com>
2023-05-01 14:04:53 +0300

    feat: add symbol input from physical and virtual keys to text area

---
+++ keyboard.js
@@ -0,0 +1,35 @@
+class Keyboard {
+  constructor(dictionary, keyContainer) {
+    this.dictionary = dictionary;
+    this.keyContainer = keyContainer;
+
+    this.createKeyboard();
+
+    this.caseState = false;
+  }
+
+  createKeyboard() {
+    for (let i = 0; i < this.dictionary.length; i += 1) {
+      let rowCount = 5;
+      while (rowCount !== this.dictionary[i].row) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        this.keyContainer.append(row);
+        rowCount = this.dictionary[i].row;
+      }
+      const button = document.createElement('button');
+      button.innerText = this.dictionary[i].keyEn;
+      button.classList.add('keyboard__key');
+      button.dataset.code = this.dictionary[i].code;
+      document.querySelectorAll('.row')[rowCount].append(button);
+    }
+  }
+
+  clickButton(code) {
+    const item = this.dictionary.find(value => value.code === code);
+    const textarea = document.querySelector('.textarea');
+    textarea.value = `${textarea.value}${item.keyEn}`;
+  }
+}
+
+export default Keyboard;
---
+++ script.js
@@ -0,0 +1,35 @@
+import dictionary from './dictionary.js';
+import Keyboard from './keyboard.js';
+
+function createPage() {
+  const body = document.querySelector('body');
+
+  const textarea = document.createElement('textarea');
+  textarea.classList.add('textarea');
+  body.appendChild(textarea);
+  const keyContainer = document.createElement('div');
+  keyContainer.classList.add('keyboard');
+  body.appendChild(keyContainer);
+
+  const keyboard = new Keyboard(dictionary, keyContainer);
+
+  function clickPhysButton(event) {
+    event.preventDefault();
+    const key = keyContainer.querySelector(`[data-code="${event.code}"]`);
+    if (key) {
+      keyboard.clickButton(event.code);
+    }
+  }
+
+  function clickVirtButton(event) {
+    const target = event.target;
+    if (target.tagName === 'BUTTON') {
+      keyboard.clickButton(target.dataset.code);
+    }
+  }
+
+  window.addEventListener('keydown', clickPhysButton);
+  keyContainer.addEventListener('click', clickVirtButton);
+}
+
+window.addEventListener('load', createPage);
